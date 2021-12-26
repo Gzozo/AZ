28db7ce2fb9948aa20b1869799a523e704ac9c45
Zoli <gallikzoltan@gmail.com>
2021-12-26 02:34:11 +0100

    Spray added

--- src/AZ/Minigun.java
+++ src/AZ/Minigun.java
@@ -1,37 +1,41 @@
 package AZ;
 
 import java.awt.Color;
+import java.util.Random;
 
 /**
  * MINIGUN
  */
 public class Minigun extends Ammo
 {
-	//TODO: Random spay
-	public Minigun(double x, double y, double rot, Field f)
-	{
-		super(x, y, 3, 2, rot, 500, f);
-		setPic("minigun.png");
-		cooldown = 1;
-		c = Color.GRAY;
-		shellCount = 50;
-	}
-	
-	public Minigun()
-	{
-		this(0, 0, 0, null);
-	}
-	
-	@Override
-	public Ammo newInstance(double x, double y, double rot, Field f)
-	{
-		return new Minigun(x, y, rot, f);
-	}
-	
-	@Override
-	public Ammo newInstance()
-	{
-		return newInstance(0, 0, 0, null);
-	}
-	
+    Random r = new Random();
+    double szoras = Settings.szoras;
+    
+    public Minigun(double x, double y, double rot, Field f)
+    {
+        super(x, y, 3, 2, rot, 500, f);
+        setPic("minigun.png");
+        cooldown = 1;
+        c = Color.GRAY;
+        shellCount = 50;
+    }
+    
+    public Minigun()
+    {
+        this(0, 0, 0, null);
+    }
+    
+    @Override
+    public Ammo newInstance(double x, double y, double rot, Field f)
+    {
+        double elter = r.nextDouble() * szoras - szoras / 2;
+        return new Minigun(x, y, rot + elter, f);
+    }
+    
+    @Override
+    public Ammo newInstance()
+    {
+        return newInstance(0, 0, 0, null);
+    }
+    
 }
